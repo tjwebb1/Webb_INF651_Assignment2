7ca48277db30518469aef3a379ffc1ece0d09501
tjwebb1 <112522859+tjwebb1@users.noreply.github.com>
2022-09-19 18:06:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// question 1
+var myNumber;
+//question 2
+myNumber = 5;
+// question 3
+var  myOtherNumber = 5;
+// question 4
+var myTotal = myNumber * myOtherNumber;
+// question 5
+var myName ="";
+var myCombo = "";
+var myCombo = myNumber + myOtherNumber;
+// question 6
+var myOtherCombo = myNumber + myOtherNumber;
+// question 7
+var myRemainder = myNumber % 5;
+// question 8
+var i = 2;
+// question 9
+var int = 4;
+// question 10
+var myBoolean = true;
